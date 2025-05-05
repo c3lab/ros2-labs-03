9357f8054c2c6b2d66fa0ef6829b9fdec742a3af
Nunzio Barone <nunzio03@hotmail.it>
2025-05-05 03:20:14 +0200

    Add files via upload

--- ros_ws/src/robot_simulation/robot_simulation/velocity_pub.py
+++ ros_ws/src/robot_simulation/robot_simulation/velocity_pub.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
+
 from poliba_interfaces.msg import UnicycleCmd, RobotStatus
+from poliba_interfaces.srv import SumProduct  
 
 class UnicycleCmdPublisher(Node):
     def __init__(self):
@@ -8,6 +10,8 @@ class UnicycleCmdPublisher(Node):
 
         # Publisher
         self.publisher_ = self.create_publisher(UnicycleCmd, '/unicycle_cmd', 10)
+
+        # Subscriber
         self.subscriber_ = self.create_subscription(
             RobotStatus,
             '/robot_status',
@@ -15,21 +19,23 @@ class UnicycleCmdPublisher(Node):
             10
         )
 
-        # Publish at 1 Hz
+        # Timer 1 Hz
         self.timer = self.create_timer(1.0, self.publish_command)
 
         # Command values
         self.linear_velocity = 1.0
         self.angular_velocity = 0.5
 
+        # Service server
+        self.service = self.create_service(SumProduct, 'sum_product', self.handle_sum_product)
+        self.get_logger().info('SumProduct service ready.')
+
     def status_callback(self, msg: RobotStatus):
-        # if the battery level is low, stop the robot
         if msg.battery_level < 30.0:
             self.linear_velocity = 0.0
             self.angular_velocity = 0.0
             self.get_logger().warning('Battery low, stopping the robot.')
         else:
-            # Resume normal operation
             self.linear_velocity = 1.0
             self.angular_velocity = 0.5
             self.get_logger().info('Battery level is sufficient, sending normal velocities.')
@@ -40,7 +46,17 @@ class UnicycleCmdPublisher(Node):
         msg.angular_velocity = float(self.angular_velocity)
 
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing UnicycleCmd: linear={msg.linear_velocity:.2f}, angular={msg.angular_velocity:.2f}')
+        self.get_logger().info(
+            f'Publishing UnicycleCmd: linear={msg.linear_velocity:.2f}, angular={msg.angular_velocity:.2f}'
+        )
+
+    def handle_sum_product(self, request, response):
+        response.sum = request.a + request.b
+        response.product = request.a * request.b
+        self.get_logger().info(
+            f'SumProduct Service called: a={request.a}, b={request.b} → sum={response.sum}, product={response.product}'
+        )
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
